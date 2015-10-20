69eb6bfe83f4aec2bee659124504726ed2177d10
Max <sync95@gmail.com>
2015-10-19 20:07:46 -0400

    Initial Commit

---
+++ CampusWalk/CampusWalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CampusWalk
+//
+//  Created by Max Marze on 10/19/15.
+//  Copyright © 2015 Max Marze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
